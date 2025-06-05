65fa4902751d75ade5a6ede224a17de47a977fa9
Kirill Lavrov <lkv@myttk.ru>
2025-06-05 22:13:04 +0700

    fixed local api url

--- src/shared/config/api.ts
+++ src/shared/config/api.ts
@@ -1,5 +1,6 @@
 export const API_URL = 'http://o-complex.com:1337';
-export const LOCAL_URL = process.env.VERCEL_URL
-? `/api` : 'http://localhost:3000/api';
+export const LOCAL_URL = `/api`;
+// export const LOCAL_URL 
+// ? `/api` : 'http://localhost:3000/api';
 export const PAGE_SIZE = 6;
 
