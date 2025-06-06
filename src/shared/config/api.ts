8b7dfa3606e45b967af2747632f8f955bbd106b5
Kirill Lavrov <lkv@myttk.ru>
2025-06-07 03:42:49 +0700

    changed local url

--- src/shared/config/api.ts
+++ src/shared/config/api.ts
@@ -1,5 +1,5 @@
 export const API_URL = 'http://o-complex.com:1337';
-export const LOCAL_URL = process.env.VERCEL_URL
+export const LOCAL_URL = process.env.NODE_ENV === "production"
 ? `https://o-complex-pi.vercel.app/api` : 'http://localhost:3000/api';
 export const PAGE_SIZE = 6;
 
